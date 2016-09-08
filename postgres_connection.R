80120fe776e8ba17e0e553dcebcb1aa0e8b1a468
Damien Caillaud <caillaud@DSS04766-ANT-D.local>
2016-09-08 14:55:55 -0700

    minor update

--- postgres_connection.R
+++ postgres_connection.R
@@ -6,8 +6,28 @@ return(temp)
 }
 
 
-# createListSQLTables <- function(listTables){
-	# create table 
+
+#listTables <- jsonOutputConversion(NULL, "~/Documents/git repositories/temp/behaviors.json", "~/Documents/git repositories/temp/layout_info.json")
+
+createListSQLTables <- function(listTables){
+	#list of headers
+	tableHeaders <- list()
+	tableHeaders[[1]] <- names(listTables$sessionsTable)
+	tableHeaders[[2]] <- names(listTables$focalsTable)
+	tableHeaders[[3]] <- names(listTables$behaviorsTable)
+	tableHeaders[[4]] <- names(listTables$scansTable)
+	tableHeaders[[5]] <- names(listTables$backgroundTapsTable)
+	tableHeaders[[6]] <- names(listTables$commentsTable)
+	tableHeaders[[7]] <- names(listTables$dayVarsTable)
+	tableHeaders[[8]] <- names(listTables$focalVarsTable)
+	tableHeaders[[9]] <- names(listTables$continuousVarsTable)
+	tableHeaders[[10]] <- names(listTables$scanVarsTable)
+	sqlCode <- list()
+	sqlCode <- c(sqlCode, "create schema animal_observer;")
+	sqlCode <- c(sqlCode, paste("create schema animal_observer;"))
+
 	
-# }
+}
+
+
 
