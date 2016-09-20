3c172e14a1caa9775e533440582e63d62b424b5f
Damien <dcaillaud@gorillafund.org>
2016-09-19 22:43:33 -0700

    handles case where primary key is already in database

--- postgres_upload.R
+++ postgres_upload.R
@@ -2,12 +2,11 @@ timeFormat <- function(stamp){
 	paste(unlist(strsplit(stamp, ",")), collapse=" ")
 }
 
-
 uploadDatFile <- function(dataOutput, con){
 	sessionsTable <- dataOutput$sessionsTable
 	for (i in 1:nrow(sessionsTable)){
 		dbGetQuery(con, paste("INSERT INTO main_tables.list_sessions(device_ID, session_start_time, session_end_time, group_ID, pin_code_name, layout_info_json_version, behaviors_json_version, gps_on, compass_on, map_mode_on)
-    VALUES(
+    SELECT 
     '",as.character(sessionsTable[i,]$device_ID),"',
     '",timeFormat(as.character(sessionsTable[i,]$session_start_timeStamp)),"',
     '",timeFormat(as.character(sessionsTable[i,]$session_end_timeStamp)),"',
@@ -18,9 +17,8 @@ uploadDatFile <- function(dataOutput, con){
     '", as.character(sessionsTable[i,]$gps_on),"',
     '", as.character(sessionsTable[i,]$compass_on),"',
     '", as.character(sessionsTable[i,]$map_mode_on),"'
-	);", sep="")
+	WHERE NOT EXISTS (SELECT 1 from main_tables.list_sessions WHERE device_ID='",as.character(sessionsTable[i,]$device_ID),"' AND session_start_time='",timeFormat(as.character(sessionsTable[i,]$session_start_timeStamp)),"');", sep="")
 	)
 	}
 }
-
 
