ad26750ce804a2db2073b04e6cdb7b7c7555a998
FosseyFund <dcaillaud@gorillafund.org>
2017-09-08 16:36:53 -0700

    intergrate client in a sidebarlayout
    Link to uploaded files not functional yet

--- DT test/ui.R
+++ DT test/ui.R
@@ -1,5 +1,27 @@
-shinyUI(fluidPage(
-  	br(),
+shinyUI(fluidPage(   
+    sidebarLayout(
+    sidebarPanel(
+    #helpText("Upload collected data"),
+    fileInput('json.output.file', '1. Upload "Username_Date_Time.dat"'),
+	fileInput('behaviors.json', '2. Upload "behaviors.json"'),
+    fileInput('layout_info.json', '3. Upload "layout_info.json"'),
+    checkboxInput("colmerge", "Merge identically-named columns from dyadic and self-directed/health data", FALSE),
+	    HTML('<hr style="height:1px;border:none;color:#333;background-color:#333;" />'),
+    downloadButton("downloadZip", label = "Download all data as zip"),
+    br(), br(), h5("    OR"), br(),      
+      downloadButton('downloadSessionsTable', 'Download list of sessions'),
+      downloadButton('downloadFocalsTable', 'Download list of focals'),
+      downloadButton('downloadBehaviorsTable', 'Download list of behaviors'),
+	  downloadButton('downloadScansTable', 'Download list of scans'),
+	  downloadButton('downloadBackgroundTapsTable', 'Download list of background taps'),
+	  downloadButton('downloadCommentsTable', 'Download list of comments'),
+	  downloadButton('downloadDayVarsTable', 'Download day variables'),
+	  downloadButton('downloadFocalVarsTable', 'Download focal variables'),
+	  downloadButton('downloadContinuousVarsTable', 'Download global variables'),
+  	  downloadButton('downloadScanVarsTable', 'Download scan variables')
+    ),
+    mainPanel(
+      br(),
   	downloadButton("downloadBehaviorsView", "Download behaviors", icon=icon('download'), style="color: #090909; background-color: #cdcdcd; border-color: #090909"),
   	downloadButton("downloadScansView", "Download scans", icon=icon('download'), style="color: #090909; background-color: #cdcdcd; border-color: #090909"),
   	HTML("<h3><b>    Sessions</b></h3>"),
@@ -47,5 +69,40 @@ shinyUI(fluidPage(
 	actionButton('deleteScanRow', 'Delete selected row', icon=icon('close'), style="color: #090909; background-color: #cdcdcd; border-color: #090909"),    
     hr(),
     hr()
-  ))
-  
+)
+)
+)
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
