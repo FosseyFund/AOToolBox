5d3562dfa54c721cefc7bde7947cd8f8132dbdf1
FosseyFund <dcaillaud@gorillafund.org>
2017-09-18 12:58:11 -0700

    add D3TableFilter loading

--- DT test/global.R
+++ DT test/global.R
@@ -10,12 +10,12 @@ install.load("rhandsontable")
 install.load("markdown")
 install.load("RPostgreSQL")
 install.load("gtools")
-
+install.load("devtools")
+if (!require("D3TableFilter", character.only=T)) devtools::install_github("ThomasSiegmund/D3TableFilter")
+library(D3TableFilter)
 library(shiny)
 source("../jsonOutputConversion.R")
 options(shiny.maxRequestSize=20*1024^2)
-
-#con <- dbConnect(drv=dbDriver("PostgreSQL"), dbname = "aotest2", host = "localhost", port = 5432, user = "postgres", password = "postgres")
 
 library(htmlwidgets)
 library(D3TableFilter)
@@ -24,6 +24,3 @@ library(D3TableFilter)
   		temp[grepl("NA", temp)] <- NA
   		return(temp)
   	}
-# dat1 <- data.frame(dbGetQuery(con, "select * from main_tables.all_focal_data_view;"))
-# dat1[,unlist(lapply(dat1, function(x) inherits(x, "POSIXt")))] <- apply(dat1[,unlist(lapply(dat1, function(x) inherits(x, "POSIXt")))], 2, timeToChar)
-# dat2 <- data.frame(dbGetQuery(con, "select * from main_tables.all_scan_data_view;"))
